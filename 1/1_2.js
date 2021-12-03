f7e6989b4a639651cf3276fa9af3c87a8e425534
Maxman013 <mc.maxman013@gmail.com>
2021-12-03 15:58:35 +1000

    Update 1_2.js
    Finished 1_2

--- 1/1_2.js
+++ 1/1_2.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 var measurements = fs.readFileSync("1.txt", {encoding: "utf8"}).split("\n").map(Number);
 var count = 0;
 
-for (i = 1; i < measurements.length; i++) {
-    if (measurements[i] > measurements[i - 1]) {
+for (i = 3; i < measurements.length; i++) {
+    if (measurements[i] + measurements[i - 1] + measurements[i - 2] > measurements[i - 1] + measurements[i - 2] + measurements[i - 3]) {
         count++;
     }
 }
