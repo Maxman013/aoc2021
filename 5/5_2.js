52b132b7f8a1dc07b5782a31ac89363364b7e3ab
Maxman013 <mc.maxman013@gmail.com>
2021-12-05 18:54:30 +1000

    Create 5_2.js
    Not too hard just had to add lots of cases

---
+++ 5/5_2.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+var input = fs.readFileSync("5.txt", {encoding: "utf8"}).split('\n');
+
+// populate 1000x1000 area with 0s, number indicates number of crossings
+var area = [];
+for (i = 0; i < 1000; i++) {
+    area[i] = [];
+    for (j = 0; j < 1000; j++) {
+        area[i][j] = 0;
+    }
+}
+
+for (i = 0; i < input.length; i++) {
+    var line = input[i].split(' ');
+    var pos1 = line[0].split(',').map(Number);
+    var pos2 = line[2].split(',').map(Number);
+
+    if (pos1[0] == pos2[0]) {
+        for (j = Math.min(pos1[1], pos2[1]); j < Math.max(pos1[1], pos2[1]) + 1; j++) {
+            area[pos1[0]][j]++;
+        }
+    } else if (pos1[1] == pos2[1]) {
+        for (j = Math.min(pos1[0], pos2[0]); j < Math.max(pos1[0], pos2[0]) + 1; j++) {
+            area[j][pos1[1]]++;
+        }
+    } else if (pos1[0] < pos2[0]) {
+        if (pos1[1] < pos2[1]) {
+            for (j = 0; j < pos2[0] - pos1[0] + 1; j++) {
+                area[pos1[0] + j][pos1[1] + j]++;
+            }
+        } else {
+            for (j = 0; j < pos2[0] - pos1[0] + 1; j++) {
+                area[pos1[0] + j][pos1[1] - j]++;
+            }
+        }
+    } else {
+        if (pos1[1] < pos2[1]) {
+            for (j = 0; j < pos1[0] - pos2[0] + 1; j++) {
+                area[pos1[0] - j][pos1[1] + j]++;
+            }
+        } else {
+            for (j = 0; j < pos1[0] - pos2[0] + 1; j++) {
+                area[pos1[0] - j][pos1[1] - j]++;
+            }
+        }
+    }
+}
+
+var counter = 0;
+for (i = 0; i < 1000; i++) {
+    for (j = 0; j < 1000; j++) {
+        if (area[i][j] > 1) {
+            counter++;
+        }
+    }
+}
+
+console.log(counter);
